e7f05f28e19bf8ef799e81a1a4188a8c0c294320
gregoryhoag <gryanhoag@gmail.com>
2021-12-06 17:11:05 -0500

    Amazing Store start

---
+++ AmazingStore/src/main/java/com/cogent/amazingstore/AmazingStoreApplication.java
@@ -0,0 +1,13 @@
+package com.cogent.amazingstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazingStoreApplication.class, args);
+	}
+
+}
---
+++ AmazingStore/src/test/java/com/cogent/amazingstore/AmazingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogent.amazingstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
