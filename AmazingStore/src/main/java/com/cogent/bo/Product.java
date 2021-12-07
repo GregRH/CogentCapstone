44813630886bb0ca70349a2d8df851d31b876e73
gregoryhoag <gryanhoag@gmail.com>
2021-12-07 11:40:00 -0500

    Product bo

--- AmazingStore/src/main/java/com/cogent/bo/Product.java
+++ AmazingStore/src/main/java/com/cogent/bo/Product.java
@@ -1,6 +1,26 @@
 package com.cogent.bo;
 
-public class Product {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.ToString;
+@Data
+@ToString
+@Entity
+@Table(name="product")
+public class Product implements Serializable {
+	@Id
+	private Long id;
+	private String description;
+	private String img;
+	private String category;
+	private long quantity;
+	private float rating;
+	private float price;
 	
 
 }
