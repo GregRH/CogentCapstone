33b8b725edc4319e75433493d12cf3e4ad6342f4
gregoryhoag <gryanhoag@gmail.com>
2021-12-13 17:15:05 -0500

    added code for some buyer and seller views

--- Angular2/AmazingStore/src/app/pages/buyer-product-view/buyer-product-view.component.ts
+++ Angular2/AmazingStore/src/app/pages/buyer-product-view/buyer-product-view.component.ts
@@ -1,15 +1,139 @@
-import { Component, OnInit } from '@angular/core';
-
+//import { createR3ProviderExpression } from '@angular/compiler';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Account } from '../../services/account';
+import { AccountService } from '../../services/account.service';
+import { Order } from '../../services/order';
+import { OrderService } from '../../services/order.service';
+import { Product } from '../../services/product';
+import { ProductService } from '../../services/product.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-buyer-product-view',
   templateUrl: './buyer-product-view.component.html',
-  styleUrls: ['./buyer-product-view.component.css']
+  styleUrls: ['./buyer-product-view.component.css'],
+	providers: [ ProductService ],
 })
 export class BuyerProductViewComponent implements OnInit {
+tiles: Tile[] = [
+    { text: 'One', cols: 5, rows: 4, color: '' },
+    { text: 'Two', cols: 5, rows: 4, color: '' },
+    { text: 'Three', cols: 2, rows: 2, color: '' },
+  ];
+	longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+  product!: Product;
+  id!: number;
+  selectedProduct: Product[] = [];
+  signedAccount!: Account;
+  newOrder = new Order();
+  orders!: Order[];
+  currentOrder!: Order;
+  updatedOrder!: Order;
+  isBuyer = true;
+  accountId: any = sessionStorage.getItem('id')
+    ? sessionStorage.getItem('id')
+    : 0;
+  signedId: number = this.accountId;
 
-  constructor() { }
+  date: string = `${new Date().getMonth()}${new Date().getDate()}${new Date().getFullYear()}`;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private ps: ProductService,
+    private os: OrderService,
+    private as: AccountService) { }
+	 //openSnackBar() {
+  //  this._snackBar.openFromComponent(ToastComponent, {
+ //     duration: this.durationInSeconds * 1000,
+//    });
+//  }
+
 
   ngOnInit(): void {
+	  this.getAccountById(this.signedId);
+    this.getOrdersByAccountId(this.signedId);
+
+    this.id = this.route.snapshot.params['id'];
+    this.getProductById(this.id);
+  }
+	getAccountById(id: number) {
+    this.as.getAccountById(id).subscribe((res) => {
+      this.signedAccount = res;
+      this.isBuyer =
+        this.signedAccount.accountType.toLowerCase() === 'seller'
+          ? false
+          : true;
+    });
+  }
+
+	newOrderDetails() {
+    this.newOrder.paid = false;
+    this.newOrder.shipped = false;
+    this.newOrder.shippingAddress = '';
+    this.newOrder.timestamp = this.date;
+    this.newOrder.account = this.signedAccount;
+    this.newOrder.products = this.selectedProduct;
+  }
+
+  getProductById(id: number) {
+    this.ps.findProductById(id).subscribe((res) => (this.product = res));
+  }
+
+  onAddToCart(product: Product) {
+    this.selectedProduct.push(product);
+    // console.log(this.selectedProduct);
+    if (!this.currentOrder) {
+      this.createOrder(this.newOrder);
+    } else {
+      this.currentOrder.paid === true
+        ? this.createOrder(this.newOrder)
+        : this.updateOrder(product);
+    }
+    //this.openSnackBar();
+  }
+	updateOrder(product: Product) {
+    this.os.getOrderByTime(this.currentOrder.timestamp).subscribe((res) => {
+      this.updatedOrder = res;
+      this.updatedOrder.products.push(product);
+      console.log(this.updatedOrder);
+      this.sharedDataService.changeMessage(this.updatedOrder.products.length);
+      this.os
+        .updateOrder(this.updatedOrder.id, this.updatedOrder)
+        .subscribe((res) => {
+          // this.log(1);
+          console.log(res);
+        });
+    });
+  }
+
+  getOrdersByAccountId(id: number) {
+    this.os.findByAccountId(id).subscribe((res) => {
+      this.orders = res;
+      console.log(res);
+      this.currentOrder = this.orders[this.orders?.length - 1];
+      console.log(this.currentOrder);
+      // this.selectedProduct = this.currentOrder.products;
+      // console.log(this.selectedProduct);
+      // this.getProducts(this.orders);
+      // this.sumOrdersPrice(this.unPaidProducts);
+    });
+  }
+
+  createOrder(order: Order) {
+    this.newOrderDetails();
+    console.log(JSON.stringify(this.newOrder));
+    this.os.createOrder(order).subscribe(
+      (data) => console.log(data),
+      (e) => console.log(e)
+    );
   }
 
 }
