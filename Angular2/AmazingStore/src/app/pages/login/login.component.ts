32e4f453fd1aa88bf60c2f4184fa6fd08b47c42c
Kashif Khan <khankashif3019@gmail.com>
2021-12-13 10:32:34 -0500

    pushing angular files

--- Angular2/AmazingStore/src/app/pages/login/login.component.ts
+++ Angular2/AmazingStore/src/app/pages/login/login.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
+  public user = {
+    id: '',
+    name: '',
+    email: '',
+    password: '',
+    dob: '',
+  };
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  formSubmit() {
+    console.log(this.user);
+    if (this.user.name == '' || this.user.name == null) {
+      alert('user name is required');
+    }
   }
-
 }
